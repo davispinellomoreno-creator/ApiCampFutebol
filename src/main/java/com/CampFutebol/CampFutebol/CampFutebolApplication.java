96a3be8f23acb8da80b0b3144c36025dc78f8365
davispinellomoreno-creator <davispinellomoreno@gmail.com>
2026-01-25 22:46:42 -0300

    first commit

---
+++ src/main/java/com/CampFutebol/CampFutebol/CampFutebolApplication.java
@@ -0,0 +1,13 @@
+package com.CampFutebol.CampFutebol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampFutebolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampFutebolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/CampFutebol/CampFutebol/CampFutebolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CampFutebol.CampFutebol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampFutebolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
