d2115022a280eeaad47c49fdfc61f2651b7b96a8
davispinellomoreno-creator <davispinellomoreno@gmail.com>
2026-01-26 22:24:08 -0300

    Segundo Commit: Estruturando o projeto ainda

--- src/main/java/com/CampFutebol/CampFutebol/Infrasctuture/Entitys/Times.java
+++ src/main/java/com/CampFutebol/CampFutebol/Infrasctuture/Entitys/Times.java
@@ -1,4 +1,23 @@
 package com.CampFutebol.CampFutebol.Infrasctuture.Entitys;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "Cadastro_Time")
 public class Times {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "nome" , unique = true)
+    private String nome;
+
+
 }
