ea0e34ae4ce5d1b4c93e48fdfde0796eb04f75b3
davispinellomoreno-creator <davispinellomoreno@gmail.com>
2026-01-31 22:53:13 -0300

    Finalizando a arquitetura do projeto, falatando agora a estrutura/base do projeto

---
+++ src/main/java/com/CampFutebol/CampFutebol/Controller/CampController.java
@@ -0,0 +1,9 @@
+package com.CampFutebol.CampFutebol.Controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/CAMP")
+public class CampController {
+}
