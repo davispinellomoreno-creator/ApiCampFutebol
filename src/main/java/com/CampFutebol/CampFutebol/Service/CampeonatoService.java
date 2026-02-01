d820b2857d4f4465ec3e22ff0dc8a37e5ef3dbea
davispinellomoreno-creator <davispinellomoreno@gmail.com>
2026-01-31 23:14:27 -0300

    Pequeno commit, para organizar o service

--- src/main/java/com/CampFutebol/CampFutebol/Service/CampeonatoService.java
+++ src/main/java/com/CampFutebol/CampFutebol/Service/CampeonatoService.java
@@ -1,7 +1,15 @@
 package com.CampFutebol.CampFutebol.Service;
 
+import com.CampFutebol.CampFutebol.Infrasctuture.Entitys.Camp;
+import com.CampFutebol.CampFutebol.Infrasctuture.Entitys.Times;
+import com.CampFutebol.CampFutebol.Infrasctuture.Repository.RepositoryCampeonato;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CampeonatoService {
+
+    private RepositoryCampeonato repositorycamp;
+    public void salvarCamp(Camp camp){
+        repositorycamp.saveAndFlush(camp);
+    }
 }
