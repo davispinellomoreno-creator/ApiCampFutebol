e4170834ae585649905de898f7b02c793ff96734
davispinellomoreno-creator <davispinellomoreno@gmail.com>
2026-02-01 23:18:30 -0300

    Commit adicional

--- src/main/java/com/CampFutebol/CampFutebol/Controller/TimeController.java
+++ src/main/java/com/CampFutebol/CampFutebol/Controller/TimeController.java
@@ -1,12 +1,11 @@
 package com.CampFutebol.CampFutebol.Controller;
 
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
 @RequestMapping("/CampFutebol")
-public class CampFutebolController {
+public class TimeController {
 }
--- src/main/java/com/CampFutebol/CampFutebol/Service/CampeonatoService.java
+++ src/main/java/com/CampFutebol/CampFutebol/Service/CampeonatoService.java
@@ -1,19 +1,25 @@
 package com.CampFutebol.CampFutebol.Service;
 
 import com.CampFutebol.CampFutebol.Infrasctuture.Entitys.Camp;
-import com.CampFutebol.CampFutebol.Infrasctuture.Entitys.Times;
 import com.CampFutebol.CampFutebol.Infrasctuture.Repository.RepositoryCampeonato;
 import org.springframework.stereotype.Service;
+
+import java.sql.Time;
 
 @Service
 public class CampeonatoService {
 
     private RepositoryCampeonato repositorycamp;
+
+    public CampeonatoService(RepositoryCampeonato repositorycamp) {
+        this.repositorycamp = repositorycamp;
+    }
+
     public void salvarCamp(Camp camp){
         repositorycamp.saveAndFlush(camp);
     }
-    public void buscarPorId(Long id){
-        repositorycamp.findById(id)
+    public Camp buscarPorId(Long id){
+       return repositorycamp.findById(id)
                 .orElseThrow(
                         ()-> new RuntimeException("Time não encontrado")
                 );
@@ -21,7 +27,7 @@ public class CampeonatoService {
     public void deletarTime(Long id){
         repositorycamp.deleteById(id);
     }
-    public void atualizarTime(Long id){
+    public void atualizarTime(Long id, Time time){
         repositorycamp.findById(id);
     }
 }
