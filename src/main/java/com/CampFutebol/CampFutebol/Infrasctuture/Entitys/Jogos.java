d0808e769de3d6250819fa09f523c7491d46d2ac
davispinellomoreno-creator <davispinellomoreno@gmail.com>
2026-02-09 13:44:24 -0300

    Implementado entidades da classe jogos

--- src/main/java/com/CampFutebol/CampFutebol/Infrasctuture/Entitys/Jogos.java
+++ src/main/java/com/CampFutebol/CampFutebol/Infrasctuture/Entitys/Jogos.java
@@ -1,7 +1,6 @@
 package com.CampFutebol.CampFutebol.Infrasctuture.Entitys;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.*;
 
 @Getter
@@ -12,4 +11,11 @@ import lombok.*;
 @Entity
 @Table(name = "Cadastro_Jogos")
 public class Jogos {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY )
+    private Long id;
+
+    private String timefora;
+    private String timecasa;
+    private Integer pontos;
 }
