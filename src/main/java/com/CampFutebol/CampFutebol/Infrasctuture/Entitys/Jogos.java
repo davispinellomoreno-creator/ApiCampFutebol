431728c36b622f8705faef75777d1d4e54bd5bb4
davispinellomoreno-creator <davispinellomoreno@gmail.com>
2026-02-08 23:07:32 -0300

    Iniciando a regra de negocio dos jogos entre os times

---
+++ src/main/java/com/CampFutebol/CampFutebol/Infrasctuture/Entitys/Jogos.java
@@ -0,0 +1,15 @@
+package com.CampFutebol.CampFutebol.Infrasctuture.Entitys;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "Cadastro_Jogos")
+public class Jogos {
+}
