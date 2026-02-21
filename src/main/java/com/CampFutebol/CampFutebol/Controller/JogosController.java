d76cb98a1a9b7856582823398d084db0c3cb62fb
davispinellomoreno-creator <davispinellomoreno@gmail.com>
2026-02-20 23:01:24 -0300

    Outro commit para começar a criar a regra de negocio do service

--- src/main/java/com/CampFutebol/CampFutebol/Controller/JogosController.java
+++ src/main/java/com/CampFutebol/CampFutebol/Controller/JogosController.java
@@ -3,7 +3,10 @@ package com.CampFutebol.CampFutebol.Controller;
 import com.CampFutebol.CampFutebol.Infrasctuture.Entitys.Jogos;
 import com.CampFutebol.CampFutebol.Infrasctuture.Repository.RepositoryJogos;
 import com.CampFutebol.CampFutebol.Service.JogosService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -15,5 +18,10 @@ public class JogosController {
     public JogosController(JogosService jogoService, RepositoryJogos repositoryJogos, Jogos jogos) {
         this.jogoService = jogoService;
         this.jogos = jogos;
+    }
+    @GetMapping
+    ResponseEntity<Jogos> buscarJogo (@RequestParam Long id){
+        return ResponseEntity.ok();
+
     }
 }
