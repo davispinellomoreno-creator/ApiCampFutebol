4f8b6de2d3f5932703694d3d60c7bb00af5de6a9
davispinellomoreno-creator <davispinellomoreno@gmail.com>
2026-02-21 22:55:26 -0300

    Commit voltado na implementação do controller, amanhã nao somente irei finalizar o controller como o projeto

--- src/main/java/com/CampFutebol/CampFutebol/Controller/JogosController.java
+++ src/main/java/com/CampFutebol/CampFutebol/Controller/JogosController.java
@@ -1,27 +1,33 @@
 package com.CampFutebol.CampFutebol.Controller;
 
+import com.CampFutebol.CampFutebol.Infrasctuture.Entitys.Camps;
 import com.CampFutebol.CampFutebol.Infrasctuture.Entitys.Jogos;
 import com.CampFutebol.CampFutebol.Infrasctuture.Repository.RepositoryJogos;
 import com.CampFutebol.CampFutebol.Service.JogosService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/Jogos")
 public class JogosController {
     private final JogosService jogoService;
    private final Jogos jogos;
-
-    public JogosController(JogosService jogoService, RepositoryJogos repositoryJogos, Jogos jogos) {
+    private final Camps camp;
+    public JogosController(JogosService jogoService, RepositoryJogos repositoryJogos, Jogos jogos, Camps camp) {
         this.jogoService = jogoService;
         this.jogos = jogos;
+        this.camp = camp;
     }
     @GetMapping
-    ResponseEntity<Jogos> buscarJogo (@RequestParam Long id){
+    ResponseEntity<String> buscarJogo (@RequestParam Long id){
         return ResponseEntity.ok();
 
+        @PostMapping("/gerar/{campeonatoId}")
+        public ResponseEntity<String> gerarJogos(@RequestParam Long id) {
+
+            jogoService.gerarJogos(camp.getId());
+
+            return ResponseEntity.ok("Jogos gerados com sucesso!");
+        }
     }
 }
